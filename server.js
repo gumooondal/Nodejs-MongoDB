813370453dca5659260b796c8c12c06dc024736a
kim <kim@kimui-MacBookAir.local>
2024-05-05 18:45:16 +0900

    전체

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
+const bcrypt = require('bcrypt')
+const MongoStore = require('connect-mongo')
 
 app.use(methodOverride('_method')) 
 app.use(express.static(__dirname +'/public'))
@@ -18,7 +20,11 @@ app.use(session({
   secret: '암호화에 쓸 비번',
   resave : false,
   saveUninitialized : false,
-  cookie : { maxAge : 60 * 60 * 1000}
+  cookie : { maxAge : 60 * 60 * 1000},
+  store : MongoStore.create({
+    mongoUrl : 'mongodb+srv://gumooondal:1234@cluster0.k6sqf9p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    dbName : 'forum'
+  })
 }))
 
 app.use(passport.session())
@@ -48,8 +54,10 @@ passport.use(new LocalStrategy(async (입력한아이디, 입력한비번, cb) =
   if (!result) {
     return cb(null, false, { message: '아이디 DB에 없음' })
   }
-  if (result.password == 입력한비번) {
-    return cb(null, result)
+
+ 
+  if ( await bcrypt.compare(입력한비번, result.password)) {
+    return cb(null, result) 
   } else {
     return cb(null, false, { message: '비번불일치' });
   }
@@ -74,15 +82,41 @@ app.get('/login', (요청, 응답)=>{
   응답.render('login.ejs')
 }) 
 
+app.get('/login2', (요청, 응답)=>{
+  응답.render('login2.ejs')
+}) 
+
 app.post('/login', async (요청, 응답, next) => {
+  let username = 요청.body.username;
+  let result = await db.collection('user').findOne({username: username});
 
   passport.authenticate('local', (error, user, info) => {
       if (error) return 응답.status(500).json(error)
       if (!user) return 응답.status(401).json(info.message)
       요청.logIn(user, (err) => {
         if (err) return next(err)
-        응답.redirect('/')
+        응답.render('afterLogin.ejs', {user : result})
       })
   })(요청, 응답, next)
 
-}) 
+})
+
+app.get('/register', (요청, 응답)=>{
+  응답.render('register.ejs')
+})
+
+app.post('/register', async (요청, 응답)=>{
+
+  let 해시 = await bcrypt.hash(요청.body.password, 10)
+
+  await db.collection('user').insertOne({
+    username : 요청.body.username,
+    password : 해시
+  })
+  응답.redirect('/')
+})
+
+app.get('/user',async (요청, 응답) =>{
+  let result = await db.collection('user').find().toArray()
+  응답.render('user.ejs', {user : result})
+})
