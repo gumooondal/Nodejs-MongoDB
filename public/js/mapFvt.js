1cb93971838ecf61f0a8216a42c1ddeec4032430
gumooondal <gumooondal@gmail.com>
2024-08-11 20:45:52 +0900

    .

--- public/js/mapFvt.js
+++ public/js/mapFvt.js
@@ -24,6 +24,7 @@ export function favoriteMarkers(fvtLocations) {
     }));
 
     // 마커 이미지의 이미지 주소입니다
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
     for (var i = 0; i < positions.length; i++) {
 
